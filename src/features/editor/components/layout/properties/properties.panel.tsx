824d1ba6e25d3bb8044bf394ed9389606f2f8cd4
kbautista2443-pixel <kbautista2443@iskwela.psau.edu.ph>
2026-05-06 05:11:17 +0800

    Implement dropdown menu for default template selection
    Added a dropdown menu for selecting default templates with options for various certificates.

--- src/features/editor/components/layout/properties/properties.panel.tsx
+++ src/features/editor/components/layout/properties/properties.panel.tsx
@@ -3,12 +3,54 @@ import { useCanvasStore } from "../../../lib/stores/canvas-store";
 import { Panel } from "../../panels/editor-panel";
 import { ObjectProperties } from "./object.properties";
 import { PageProperties } from "./page.properties";
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuItem,
+} from "@/components/ui/dropdown-menu";
+import { insertImage } from "@/features/editor/helper/insert-default-template";
 
 const Properties = () => {
+  const canvas = useCanvasStore((s) => s.canvas);
   return (
     <Panel className="top-1/2 right-2 max-w-60 min-w-60 -translate-y-1/2">
-      <ScrollArea className="block h-115 w-full space-y-4 p-2">
+      <ScrollArea className="block h-120 w-full space-y-4 p-2">
         <Settings />
+        <DropdownMenu>
+          <DropdownMenuTrigger className="mt-4 shadow-md p-1 border rounded w-full">
+            Select Default Template
+          </DropdownMenuTrigger>
+
+          <DropdownMenuContent>
+            <DropdownMenuItem
+              onClick={() => canvas && insertImage(canvas, "folderA")}>
+              Certificate of Achievement
+            </DropdownMenuItem>
+
+            <DropdownMenuItem
+              onClick={() => canvas && insertImage(canvas, "folderB")}>
+              Certificate of Participation
+            </DropdownMenuItem>
+
+            <DropdownMenuItem
+              onClick={() => canvas && insertImage(canvas, "folderC")}>
+              Certificate of Completion
+            </DropdownMenuItem>
+
+            <DropdownMenuItem
+              onClick={() => canvas && insertImage(canvas, "folderD")}>
+              Certificate of Recognition
+            </DropdownMenuItem>
+
+            <DropdownMenuItem
+              onClick={() => canvas && insertImage(canvas, "folderE")}>
+              Certificate of Appreciation
+            </DropdownMenuItem>
+
+          </DropdownMenuContent>
+
+        </DropdownMenu>
       </ScrollArea>
     </Panel>
   );
